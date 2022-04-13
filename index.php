54f69e35bbd6e12dbf21e0cba70a2d6f7e026354
Vasilache Ionut <vasilacheorionut@gmail.com>
2022-04-13 13:33:19 +0200

    add to home my work

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
         <div class="home-content">
             <h1>Welcome, I'm <br> <span class="fn">Vasilache</span> <span class="ln">Ionut</span></h1>            
             <p>I am a freelance, Web Developer, Programmer</p>
-            <a href="contact.php" class="btn">How To Reach Me</a>
+            <a href="contact.php" class="btn">How To Reach Me</a><br>
+            <a href="mywork.php"  class="btn btn-info">My Work</a>
         </div>
     </section>
 
